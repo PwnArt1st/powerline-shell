075f1427ccfaad5ee0ce6b35d746d2e38690584c
Pierre Grabolosa <pgrabolosa@gmail.com>
2016-03-28 17:53:40 +0200

    * changed default configuration * the hand is a middle finger is root

--- segments/hand.py
+++ segments/hand.py
@@ -1,3 +1,6 @@
 def add_hand_segment(powerline):
-    hand = u' \U0001F449 '
+    if not os.geteuid() == 0:
+        hand = u' \U0001F449 '
+    else:
+        hand = u' \U0001F595 '
     powerline.append(hand, Color.HOSTNAME_FG, Color.HOSTNAME_BG)
